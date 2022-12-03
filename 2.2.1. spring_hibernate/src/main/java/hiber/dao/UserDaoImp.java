5c62c45c386ab2bd973aa788a50b43f0d4e3df84
JuliaNorki <109559162+JuliaNorki@users.noreply.github.com>
2022-12-03 00:38:46 -0800

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,9 +1,12 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
+   private static Session HibernateUser;
    @Autowired
    private SessionFactory sessionFactory;
 
@@ -24,6 +28,19 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+   private static void getSessionFactory() {
+
+      try(Session session = HibernateUser.getSessionFactory().openSession()) {
+
+         String HQL= "FROM model model LEFT OUTER JOIN FETCH model.car WHERE model.model=:model";
+         Model model = session.createQuery(HQL, model.class).setParameter("model").uniqueResult();
+         System.out.println(model);
+         System.out.println(model.getSessionFactory());
+      } catch (HibernateException e) {
+         e.printStackTrace();
+      }
+
    }
 
 }
